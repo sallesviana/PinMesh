2dc67a025b840441b7281730adddff8d8d1020b4
Salles <sallesviana@gmail.com>
2016-05-18 23:44:23 -0400

    Added script to download datasets; updated README files with more instructions for the reprod. stamp committee

---
+++ smiDownloadDependenciesAndCompile.sh
@@ -0,0 +1,20 @@
+echo "---------------------------------------------"
+echo "Downloading sample data set"
+echo "---------------------------------------------"
+wget https://www.dropbox.com/s/2pqggvfhvks29tm/datasets.tar.gz?dl=1 -O datasets.tar.gz
+
+echo "---------------------------------------------"
+echo "Extracting sample data set"
+echo "---------------------------------------------"
+tar -xzf datasets.tar.gz
+
+echo "---------------------------------------------"
+echo "Installing g++ (please, run this shell script as root to ensure the dependencies will be installed)"
+echo "---------------------------------------------"
+apt-get install g++4.8
+apt-get install libgmp3-dev
+
+echo "---------------------------------------------"
+echo "Compiling PinMesh"
+echo "---------------------------------------------"
+make
