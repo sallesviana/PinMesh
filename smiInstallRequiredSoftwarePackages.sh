d4e499047458567320e2632b8ea57d47fcba6481
Salles <sallesviana@gmail.com>
2016-05-21 13:48:37 -0400

    updated README and scripts to automatically install dependencies

---
+++ smiInstallRequiredSoftwarePackages.sh
@@ -0,0 +1,5 @@
+echo "---------------------------------------------"
+echo "Installing g++ (please, provide the password for root in order to install the packages)"
+echo "---------------------------------------------"
+#sudo apt-get install g++4.8
+sudo apt-get install libgmp3-dev
